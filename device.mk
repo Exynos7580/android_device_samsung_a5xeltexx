0f7283db8ce14cd14c7df7be624daf83657af8e0
Apavayan <info@apavayan.com>
2016-05-21 13:00:17 +0530

    a5xelte: Add device specfic wifi config
    Change-Id: Ice1339bf3f1c41d5c3849074e4a7751ff5c5b944
    Signed-off-by: Apavayan <info@apavayan.com>

--- device.mk
+++ device.mk
@@ -93,6 +93,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.nfc.sec_hal=true
 
+# Wi-fi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/cred.conf:system/etc/wifi/cred.conf \
+    $(LOCAL_PATH)/configs/wifi/olsrd.conf:system/etc/wifi/olsrd.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf
 
 # SYMBOLS FOR BLOBS
 PRODUCT_PACKAGES += \
