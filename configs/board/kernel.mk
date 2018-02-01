8ed06a2355371f3896a2d54a11431fa33ca5322a
Stricted <info@stricted.net>
2018-02-01 18:17:53 +0100

    a5xeltexx: build dtb from source
    Change-Id: I614e6f32780993b9188397a6c6f6af7527d813d5

--- configs/board/kernel.mk
+++ configs/board/kernel.mk
@@ -1,8 +1,12 @@
-# Kernel
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dt.img --board SRPOJ08A000KU
-TARGET_KERNEL_CONFIG := lineageos_a5xelte_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/exynos7580-common
+# Boot
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPOJ08A000KU
 BOARD_CUSTOM_BOOTIMG := true
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+
+# Kernel
+#BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
+TARGET_KERNEL_CONFIG := lineageos_a5xelte_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos7580-common
+BOARD_KERNEL_IMAGE_NAME := Image
