40e771a5cbb382f205b998d2d0ae83e97a8a53f0
Victor Lourme <victorlourme@vlour.me>
2018-02-03 01:58:42 +0100

    a5xeltexx: init: update variant table

--- init/init_sec.cpp
+++ init/init_sec.cpp
@@ -86,26 +86,26 @@ void vendor_load_properties()
 	if (bootloader.find("A510F") != std::string::npos) {
 
 		/* SM-A510F */
-        property_override("ro.build.fingerprint", "samsung/a5xeltexx/a5xelte:5.1.1/LMY47X/A510FDXXS2APD1:user/release-keys");
-        property_override("ro.build.description", "a5xeltexx-user 5.1.1 LMY47X A510FDXXS2APD1 release-keys");
+        property_override("ro.build.fingerprint", "samsung/a5xeltexx/a5xelte:7.0/NRD90M/A510FXXU5CRA4:user/release-keys");
+        property_override("ro.build.description", "a5xeltexx-user 7.0 NRD90M A510FXXU5CRA4 release-keys");
         property_override("ro.product.model", "SM-A510F");
-        property_override("ro.product.device", "a5xelte");
+        property_override("ro.product.device", "a5xeltexx");
 
     } else if (bootloader.find("A510M") != std::string::npos) {
 
 		/* SM-A510M */
-        property_override("ro.build.fingerprint", "samsung/a5xeltexx/a5xelte:5.1.1/LMY47X/A510FXXS2APD1:user/release-keys");
-        property_override("ro.build.description", "a5xeltexx-user 5.1.1 LMY47X A510FXXS2APD1 release-keys");
+        property_override("ro.build.fingerprint", "samsung/a5xelteub/a5xelte:7.0/NRD90M/A510MUBS3CQK2:user/release-keys");
+        property_override("ro.build.description", "a5xelteub-user 7.0 NRD90M A510MUBS3CQK2 release-keys");
         property_override("ro.product.model", "SM-A510M");
-        property_override("ro.product.device", "a5xelte");
+        property_override("ro.product.device", "a5xelteub");
 
     } else {
 
 		/* SM-A510Y */
-        property_override("ro.build.fingerprint", "samsung/a5xelteub/a5xelte:5.1.1/LMY47X/A510MUBS1APC1:user/release-keys");
-        property_override("ro.build.description", "a5xelteub-user 5.1.1 LMY47X A510MUBS1APC1 release-keys");
+        property_override("ro.build.fingerprint", "samsung/a5xeltedo/a5xelte:7.0/NRD90M/A510YDOU4CQK3:user/release-keys");
+        property_override("ro.build.description", "a5xeltedo-user 7.0 NRD90M A510YDOU4CQK3 release-keys");
         property_override("ro.product.model", "SM-A510Y");
-        property_override("ro.product.device", "a5xelte");
+        property_override("ro.product.device", "a5xeltedo");
 
 	}
 
