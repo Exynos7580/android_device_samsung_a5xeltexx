487f28fd7c498236bdd6d1cc05281294df7498e9
Stricted <info@stricted.net>
2018-03-06 08:08:43 +0100

    commonize ramdisk
    Change-Id: I4c23e02b3e836a7cd846472a3072dbe6bc92779b

--- configs/product/ramdisk.mk
+++ configs/product/ramdisk.mk
@@ -20,10 +20,4 @@
 #####################
 
 PRODUCT_PACKAGES += \
-    fstab.samsungexynos7580 \
-    init.baseband.rc \
-    init.samsung.rc \
-    init.samsungexynos7580.rc \
-    init.samsungexynos7580.usb.rc \
-    init.wifi.rc \
-    ueventd.samsungexynos7580.rc
+    init.target.rc
