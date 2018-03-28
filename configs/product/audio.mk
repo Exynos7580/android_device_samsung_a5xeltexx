f306a981ce4146bdc09e835369e7e656e1185058
Stricted <info@stricted.net>
2018-03-28 16:14:54 +0200

    commonize audio_policy.conf
    Change-Id: I857591c6ea29e2addaa14aefdabd3ce9d4c9dfc3

--- configs/product/audio.mk
+++ configs/product/audio.mk
@@ -20,7 +20,6 @@
 #####################
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths_0.xml
 
 PRODUCT_PACKAGES += \
